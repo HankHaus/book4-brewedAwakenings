cf271e9d2dab4d9fbcef5669a35cd863795fab1c
Henry House <hankhaus99@gmail.com>
2022-02-28 10:03:27 -0600

    initial commit

---
+++ scripts/Orders.js
@@ -0,0 +1,62 @@
+import { getProducts, getEmployees, getOrders } from "./database.js"
+
+// Get copy of state for use in this module
+const products = getProducts()
+const employees = getEmployees()
+const orders = getOrders()
+
+
+// Function whose responsibility is to find the product for an order
+const findProduct = (order, allProducts) => {
+    let orderProduct = null
+
+    for (const product of allProducts) {
+        if (product.id === order.productId) {
+            orderProduct = product
+        }
+    }
+
+    return orderProduct
+}
+
+// Function whose responsibility is to find the employee for an order
+const findEmployee = (order, allEmployees) => {
+    let orderEmployee = null
+     for (const employee of allEmployees) {
+        if (order.employeeId === employee.id) {
+            orderEmployee = employee
+        }
+    }
+
+    return orderEmployee
+}
+
+
+export const itemsSoldByEmployee = (employee) => {
+    let count = 0;
+    for (const order of orders) {
+        if (order.employeeId === employee.id) {
+            count++
+        }
+    }
+    return count
+}
+
+
+export const Orders = () => {
+    let html = ""
+    html = "<ul>"
+
+    for (const order of orders) {
+        const product = findProduct(order, products)
+        const employee = findEmployee(order, employees)
+        
+
+        html += `<li>${product.name} was sold by ${employee.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
